fd41347d0f7c04c8e0a28ebbf5744141f0a942cf
selestrel07 <selestrel07@gmail.com>
2023-11-29 17:52:09 +0400

    add patch and delete operations for person object

--- src/main/java/ru/selestrel/learn/spring/config/MySpringMVCDispatcherServletInitializer.java
+++ src/main/java/ru/selestrel/learn/spring/config/MySpringMVCDispatcherServletInitializer.java
@@ -1,5 +1,8 @@
 package ru.selestrel.learn.spring.config;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -16,5 +19,16 @@ public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationC
     @Override
     protected String[] getServletMappings() {
         return new String[] {"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
     }
 }
