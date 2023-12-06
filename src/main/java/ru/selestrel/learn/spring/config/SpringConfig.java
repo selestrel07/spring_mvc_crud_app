8777f78af4f1b078c8680e5d2aa4b0f7f1171dfb
selestrel07 <selestrel07@gmail.com>
2023-12-06 12:56:40 +0400

    move database connection parameters to properties file

--- src/main/java/ru/selestrel/learn/spring/config/SpringConfig.java
+++ src/main/java/ru/selestrel/learn/spring/config/SpringConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -15,17 +17,21 @@ import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 import javax.sql.DataSource;
+import java.util.Objects;
 
 @Configuration
 @ComponentScan("ru.selestrel.learn.spring")
 @EnableWebMvc
+@PropertySource("classpath:database.properties")
 public class SpringConfig  implements WebMvcConfigurer {
 
     private final ApplicationContext applicationContext;
+    private final Environment environment;
 
     @Autowired
-    public SpringConfig(ApplicationContext applicationContext) {
+    public SpringConfig(ApplicationContext applicationContext, Environment environment) {
         this.applicationContext = applicationContext;
+        this.environment = environment;
     }
 
     @Bean
@@ -56,10 +62,10 @@ public class SpringConfig  implements WebMvcConfigurer {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:3004/springmvc");
-        dataSource.setUsername("selestrel");
-        dataSource.setPassword("123qwe123");
+        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("db_driver")));
+        dataSource.setUrl(environment.getProperty("db_url"));
+        dataSource.setUsername(environment.getProperty("db_username"));
+        dataSource.setPassword(environment.getProperty("db_password"));
 
         return dataSource;
     }
