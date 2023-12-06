bedc0f56edf4a8976483f294bcd9ce4621d34f24
selestrel07 <selestrel07@gmail.com>
2023-12-06 13:23:26 +0400

    test batch update vs multiple updates

--- src/main/java/ru/selestrel/learn/spring/dao/PersonDAO.java
+++ src/main/java/ru/selestrel/learn/spring/dao/PersonDAO.java
@@ -1,11 +1,15 @@
 package ru.selestrel.learn.spring.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.selestrel.learn.spring.models.Person;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -40,4 +44,52 @@ public class PersonDAO {
     public void delete(int id) {
         jdbcTemplate.update("DELETE FROM person WHERE id=?", id);
     }
+
+    /////////////////////////////////////////
+    ///// Test batch update performance /////
+    /////////////////////////////////////////
+
+    public void testMultipleUpdate() {
+        List<Person> people = create1000People();
+        long before = System.currentTimeMillis();
+        people.forEach(person -> jdbcTemplate.update("INSERT INTO person VALUES(?, ?, ?, ?)",
+                person.getId(), person.getName(), person.getAge(), person.getEmail()));
+        long after = System.currentTimeMillis();
+
+        System.out.println("Time for multiple updates for 1000 records: " + (after - before));
+    }
+
+    public void testBatchUpdate() {
+        List<Person> people = create1000People();
+        long before = System.currentTimeMillis();
+        jdbcTemplate.batchUpdate("INSERT INTO person VALUES(?, ?, ?, ?)",
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
+                        preparedStatement.setInt(1, people.get(i).getId());
+                        preparedStatement.setString(2, people.get(i).getName());
+                        preparedStatement.setInt(3, people.get(i).getAge());
+                        preparedStatement.setString(4, people.get(i).getEmail());
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return people.size();
+                    }
+                });
+        long after = System.currentTimeMillis();
+
+        System.out.println("Time for batch update with 1000 records: " + (after - before));
+    }
+
+    private List<Person> create1000People() {
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < 1000; i++) {
+            people.add(new Person(i, "Name-" + i, i % 50 + 18, String.format("test%d@test.com", i)));
+        }
+        return people;
+    }
+
+
 }
